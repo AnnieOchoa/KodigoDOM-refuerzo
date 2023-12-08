45250bc210a118788bf70b0e6fc08a9d01231ed8
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2023-12-07 21:30:20 -0600

    capturando errores

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const buscar = async() => {
-    let valor = document.getElementById("nombrePokemon").value 
-
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${valor}`) 
-        const data = await response.json() 
+const buscar = async () => {
+    let valor = document.getElementById("nombrePokemon").value
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${valor}`)
+        const data = await response.json()
         console.log(data)
-        data.results.map(() => {
-            console.log(character.name)
-        })
+        document.querySelector("#pokemon").innerHTML = `
+   <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                    card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                            </div>`;
+    } catch (error) {
+        document.querySelector("#pokemon").innerHTML = `error no se encontro el pokemon`;
+        console.log(error);
+    }
 }
